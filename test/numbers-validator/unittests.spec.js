7855e11e771a2ec37cc8b267559b87401a47bdc5
Tinatin Abuladze <tiabuladze@gmail.com>
2024-04-01 23:55:12 +0400

    done

--- test/numbers-validator/unittests.spec.js
+++ test/numbers-validator/unittests.spec.js
@@ -1,13 +1,13 @@
-import { NumbersValidator } from "../../app/numbers_validator.js";
-import { expect } from "chai";
+import {NumbersValidator} from '../../app/numbers_validator.js';
+import {expect} from 'chai';
 
-describe("getEventNumbersFromArray", () => {
+describe('getEventNumbersFromArray', () => {
   let validator;
 
   beforeEach(() => {
     validator = new NumbersValidator();
   });
-  
+
   afterEach(() => {
     validator = null;
   });
@@ -23,37 +23,38 @@ describe("getEventNumbersFromArray", () => {
   });
 
   it('should throw an error when passed value is not typeof number', () => {
-    expect(validator.isNumberEven).to.throw()});
+    expect(validator.isNumberEven).to.throw();
+  });
 
-  it("Should return an array of even numbers", () => {
+  it('Should return an array of even numbers', () => {
     const arr = [3, 4, 8, 11, 24, 20];
     const evenNumbersfromArray = validator.getEvenNumbersFromArray(arr);
     expect(evenNumbersfromArray).to.be.deep.eq([4, 8, 24, 20]);
   });
 
-  it("should return true when provided an array of all numbers", () => {
+  it('should return true when provided an array of all numbers', () => {
     const arrayOfNumbers = [3, 4, 8, 11, 24, 20];
     expect(validator.isAllNumbers(arrayOfNumbers)).to.be.true;
   });
 
-  it("Should throw an error when passed value is not array of numbers", () => {
-    const arr = ["3", "4", 8, 11, 24, 20];
+  it('Should throw an error when passed value is not array of numbers', () => {
+    const arr = ['3', '4', 8, 11, 24, 20];
     const result = validator.getEvenNumbersFromArray.bind(null, arr);
     expect(result).to.throw(`[${arr}] is not an array of "Numbers"`);
   });
 
-  it("Should throw an error when passed value is not an array", () => {
+  it('Should throw an error when passed value is not an array', () => {
     const i = {name: 'tiko'};
     expect(() => validator.isAllNumbers(i)).to.throw(`[${i}] is not an array`);
   });
 
-  it("Should return true when passed value is integer", () => {
+  it('Should return true when passed value is integer', () => {
     const i = 9;
     expect(validator.isInteger(i)).to.be.true;
-  })
+  });
 
-  it("Should throw an error when passed value is not integer", () => {
-    const i = "A";
-    expect(()=> validator.isInteger(i)).to.throw(`[${i}] is not a number`)
-  })
+  it('Should throw an error when passed value is not integer', () => {
+    const i = 'A';
+    expect(()=> validator.isInteger(i)).to.throw(`[${i}] is not a number`);
+  });
 });
